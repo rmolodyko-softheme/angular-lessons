d1bb99b97a12d358ec1cfa41f8c09452bb49ae44
ruslan.molodyko@softheme.com <ruslan.molodyko@softheme.com>
2019-09-30 16:50:33 +0300

    Initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { User, UserStatus } from './models/user';
+import { User } from './models/user';
 import { USERS } from './mock-data/users';
 
 const users = USERS;
@@ -11,7 +11,6 @@ const users = USERS;
 })
 export class AppComponent {
   users: User[] = [];
-  statusTypes = UserStatus;
   selectedUserId: number;
 
   trackByFn(index: number, user: User) {
@@ -28,15 +27,5 @@ export class AppComponent {
     console.info('Load users');
     // this.users = users;
     this.users = JSON.parse(JSON.stringify(users)); // Emulate immutable data
-  }
-
-  addPeter() {
-    users.push({
-      id: 6,
-      isVisible: true,
-      name: 'Peter',
-      status: UserStatus.Active,
-      logo: 'https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp&f=y'
-    });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { BadgeAttributiveDirective } from './badge-attributive/badge-attributive.directive';
-import { BadgeStructuralDirective } from './badge-structural/badge-structural.directive';
 import { BadgeComponent } from './badge-component/badge.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BadgeAttributiveDirective,
-    BadgeStructuralDirective,
     BadgeComponent
   ],
   imports: [
